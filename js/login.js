52b3730497aa0f2a12f5be355c64238c83ef546f
Nataly Fernandez <natyayelenfernandez@gmail.com>
2024-08-05 23:27:21 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,89 @@
+document.getElementById('form-registro').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let nombre = document.getElementById('nombre-registro').value;
+    let apellido = document.getElementById('apellido-registro').value;
+    let email = document.getElementById('email-registro').value;
+    let contrasena = document.getElementById('contrasena-registro').value;
+
+    // Verificar si el usuario ya existe
+    if (localStorage.getItem(email)) {
+        Swal.fire({
+            title: `El usuario ya existe.`,
+            icon: "info"
+        });
+    } else {
+        // Guardar el usuario en Local Storage
+        localStorage.setItem(email, JSON.stringify({ nombre, apellido, contrasena }));
+        Swal.fire({
+            title: `Usuario registrado correctamente`,
+            icon: "sucess"
+        });
+    }
+});
+
+document.getElementById('form-iniciar').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let email = document.getElementById('email').value;
+    let contrasena = document.getElementById('contrasena').value;
+    let rememberMe = document.querySelector('.remember-me').checked;
+
+    let usuario = localStorage.getItem(email);
+
+    if (usuario) {
+        let { contrasena: storedPassword } = JSON.parse(usuario);
+        if (storedPassword === contrasena) {
+            if (rememberMe) {
+                localStorage.setItem('usuarioAutenticado', email);
+            } else {
+                sessionStorage.setItem('usuarioAutenticado', email);
+            }
+            window.location.href = '../index.html';
+        } else {
+            Swal.fire({
+                title: `Contraseña incorrecta.`,
+                text: `Intenta de nuevo`,
+                icon: "error"
+            });
+        }
+    } else {
+        Swal.fire({
+            title: `Usuario no encontrado.`,
+            text: `Intenta de nuevo`,
+            icon: "error"
+        });
+    }
+});
+
+const forms = {
+    registro: document.getElementById('registro'),
+    sesion: document.getElementById('sesion')
+};
+
+// Show the form based on the hash in the URL
+const showForm = (formId) => {
+    Object.values(forms).forEach(form => form.classList.remove('active'));
+    forms[formId].classList.add('active');
+};
+
+// Check hash on load
+const hash = window.location.hash.replace('#', '');
+if (hash && forms[hash]) {
+    showForm(hash);
+} else {
+    showForm('sesion');
+}
+
+// Handle form switch
+document.querySelectorAll('.switch-form').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute('data-target');
+        showForm(targetId);
+        // Update URL hash without causing scroll
+        window.history.replaceState(null, null, `#${targetId}`);
+    });
+});
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,124 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let usuarioAutenticado = localStorage.getItem('usuarioAutenticado') || sessionStorage.getItem('usuarioAutenticado');
+
+    if (!usuarioAutenticado) {
+        window.location.href = '../pages/login.html';
+    }
+});
+
+document.querySelector('.sign-out').addEventListener('click', function() {
+    localStorage.removeItem('usuarioAutenticado');
+    sessionStorage.removeItem('usuarioAutenticado');
+    window.location.href = '../pages/login.html';
+});
+
+class Media {
+    constructor(titulo, descripcion, tipo, generos = []) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.image = `./assets/${tipo.toLowerCase()}s/${titulo.toLowerCase().replace(/ /g, '_')}.jpg`;
+        this.tipo = tipo;
+        this.genero = generos;
+    }
+}
+
+let multimedia = [
+    new Media('Damsel', 'Elodie, una hija obediente, acepta casarse con un príncipe para salvar a su pueblo, pero su boda da un giro estremecedor y ahora debe encontrar la forma de salvarse a sí misma.', 'pelicula', ['accion', 'aventura', 'fantasia']),
+    new Media('Outerbanks', '', 'serie', ['accion', 'aventura', 'fantasia']),
+    new Media('Atlas', '', 'pelicula', ['accion', 'aventura', 'fantasia']),
+    new Media('Bridgerton', '', 'serie', ['accion', 'aventura', 'fantasia']),
+    new Media('Un asunto familiar', '', 'pelicula', ['accion', 'aventura', 'fantasia']),
+    new Media('Stranger things', '', 'serie', ['accion', 'aventura', 'fantasia']),
+]
+
+let containerSlides = document.querySelector('.carousel-inner');
+let containerMedia = document.querySelector('#containerMedia');
+let containerList = document.querySelector('#containerList');
+let templateMedia = document.querySelector('.template-media').content;
+
+multimedia.slice(-3).forEach((media, index) => {
+    let copiaSlides = document.querySelector('.template-slide').content.cloneNode(true);
+    copiaSlides.querySelector('h2').textContent = media.titulo;
+    copiaSlides.querySelector('p').textContent = media.descripcion;
+    copiaSlides.querySelector('img').src = media.image;
+
+    // Establecer la primera diapositiva como activa
+    if (index === 0) {
+        copiaSlides.querySelector('.carousel-item').classList.add('active');
+    }
+
+    containerSlides.appendChild(copiaSlides);
+});
+
+multimedia.forEach((media) => {
+    let copiaMedia = document.querySelector('.template-media').content.cloneNode(true);
+    copiaMedia.querySelector('h3').textContent = media.titulo;
+    copiaMedia.querySelector('p').textContent = media.genero.join(', ');
+    copiaMedia.querySelector('img').src = media.image;
+    copiaMedia.querySelector('img').alt = media.titulo;
+    let agregarLista = copiaMedia.querySelector('.agregar-lista');
+
+    agregarLista.addEventListener('click', () => {
+        agregarALaLista(media);
+    });
+
+    containerMedia.append(copiaMedia);
+});
+
+function agregarALaLista(media) {
+    // Verificar si el medio ya está en la lista
+    if (estaEnLaLista(media)) {
+        Swal.fire({
+            title: `${media.titulo} ya esta en tu lista`,
+            icon: "info"
+        });
+        return;
+    }
+
+    let copiaLista = templateMedia.cloneNode(true);
+
+    copiaLista.querySelector('h3').textContent = media.titulo;
+    copiaLista.querySelector('p').textContent = media.genero.join(', ');
+    copiaLista.querySelector('img').src = media.image;
+
+    copiaLista.querySelector('.agregar-lista').classList.add('quitar-lista');
+    let quitarLista = copiaLista.querySelector('.quitar-lista');
+    quitarLista.classList.remove('agregar-lista');
+    quitarLista.querySelector('.fa-plus').classList.add('fa-xmark');
+    quitarLista.querySelector('.fa-xmark').classList.remove('fa-plus');
+
+    Swal.fire({
+        title: `${media.titulo} Agregado`,
+        text: `¡Agregaste este titulo a tu lista con exito!`,
+        icon: "success"
+    });
+
+    quitarLista.addEventListener('click', () => {
+        quitarDeLaLista(media);
+    });
+
+    containerList.append(copiaLista);
+}
+
+function estaEnLaLista(media) {
+    let items = containerList.querySelectorAll('.media-item');
+    for (let item of items) {
+        if (item.querySelector('h3').textContent === media.titulo) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function quitarDeLaLista(media) {
+    let items = containerList.querySelectorAll('.media-item');
+    items.forEach(item => {
+        if (item.querySelector('h3').textContent === media.titulo) {
+            containerList.removeChild(item);
+            Swal.fire({
+                title: `${media.titulo} eliminado`,
+                text: `Eliminaste este titulo de tu lista con exito`
+            });
+        }
+    });
+}
